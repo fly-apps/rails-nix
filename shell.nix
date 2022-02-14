a3ad152cb602ce194c42b78a683e523f22043a68
Samuel Dionne-Riel <samuel.dionne-riel@determinate.systems>
2022-02-14 16:41:58 -0500

    shell.nix: Make more declarative + document

--- shell.nix
+++ shell.nix
@@ -1,33 +1,52 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? (import ./nixpkgs.nix {})
+}:
 
-# Original setup without bundix
-#
-# with pkgs;
-
-# mkShell {
-#   buildInputs = [
-#     stdenv
-#     libiconv
-#     libxml2
-#     ruby_3_0
-#     postgresql_14
-#     redis
-#     bundix
-#     sqlite
-#   ];
-
-#   BUNDLE_PATH = "vendor/bundle";
-# }
-
-# Setup with bundix: first ran `bundix -l`
-with (import <nixpkgs> {});
 let
-  gems = bundlerEnv {
+  # Using `fetchGit` helps reduce pollution.
+  # This is because instead of copying the whole directory, it copies the
+  # checked-out revision + any files tracked or staged.
+  app-src = builtins.fetchGit ./.;
+in
+
+#
+# `callPackage` is the mechanism used for dependency injection within Nixpkgs.
+# It takes two parameters:
+#   - first a file containing a function, or a function.
+#     Its inputs are the desired dependencies in a set-pattern argument.
+#   - second is any overrides or additional inputs.
+#
+pkgs.callPackage (
+
+  { mkShell
+  , ruby
+  , bundlerEnv
+  , app-src
+  }:
+
+  let
+    gems = bundlerEnv {
+      name = "rails-nix-gems";
+      inherit ruby;
+      gemdir = app-src;
+    };
+  in
+  mkShell {
     name = "rails-nix";
-    inherit ruby;
-    gemdir = ./.;
-  };
-in stdenv.mkDerivation {
-  name = "rails-nix";
-  buildInputs = [gems ruby];
+    buildInputs = [
+      gems
+      ruby
+    ];
+  }
+
+) {
+  inherit app-src;
 }
+
+# If the build required, let's say, `ruby_2_6`, the last paramter could be:
+#
+# ```
+# {
+#   # ...
+#   ruby = pkgs.ruby_2_6;
+# }
+# ```
