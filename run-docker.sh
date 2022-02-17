82da64838f1d2db5f7ab8adb46a7f7e86db00d27
Samuel Dionne-Riel <samuel.dionne-riel@determinate.systems>
2022-02-17 18:07:16 -0500

    Add run-docker.sh script
    Used to run the app under a docker container.

---
+++ run-docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+PS4=" $ "
+set -x
+
+image="$(nix-build -A dockerImage)"
+image_id="$(docker load --quiet < "$image" | sed -e 's/^[^:]\+:\s*//')"
+
+exec docker \
+	run \
+	-e RAILS_LOG_TO_STDOUT=yes \
+	-e RAILS_ENV=production \
+	-e SECRET_KEY_BASE=hi \
+	-e PORT=3000 \
+	--publish 3000:3000 \
+	"$image_id"
