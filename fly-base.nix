667b805b121cf8598b694728e687b08bd2f36666
Joshua Sierles <joshua@fly.io>
2022-03-09 12:15:46 +0100

    bump to latest nix-base

--- fly-base.nix
+++ fly-base.nix
@@ -2,14 +2,14 @@
 # This can be replaced by any method of tracking inputs.
 #   - niv     https://github.com/nmattia/niv
 #   - npins   https://github.com/andir/npins
-#   - Flakes  https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake.html 
+#   - Flakes  https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake.html
 #
 # Using this simple purely-Nix shim serves as a placeholder.
 
 let
   # Tracking https://github.com/fly-apps/nix-base/pull/3
-  rev = "cdd35d244ce3b21bc2910e086da517e361744457";
-  sha256 = "sha256:1i1npj0qxmf9kvf786qnrcqkr24286kzi99a9j0vii16qffv178f";
+  rev = "5942668b5c0054a4dd962f93e42dd066c80da417";
+  sha256 = "sha256:0anvxk77b57dgir5l7zmgz02j3rkqibzfnk0bb6dla1i0dw6hbmy";
   tarball = builtins.fetchTarball {
     url = "https://github.com/fly-apps/nix-base/archive/${rev}.tar.gz";
     inherit sha256;
