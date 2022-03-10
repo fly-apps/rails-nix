32f50f7225bb603f2fc9e8d002070835616eaac5
Samuel Dionne-Riel <samuel.dionne-riel@determinate.systems>
2022-03-10 16:04:36 -0500

    default.nix: Add example of templates options

--- default.nix
+++ default.nix
@@ -7,6 +7,16 @@
     runtimes.ruby.version = "3.1.1";
     runtimes.ruby.withJemalloc = false;
 
+    templates.rails.assetInputs = [
+      # If this dir existed, it would also be used for the assets pre-compilation
+      "app/somedir"
+    ];
+
+    templates.rails.gemInputs = [
+      # If this dir existed, it would also be used for the gems build
+      "src/gem_dependency"
+    ];
+
     # This could also be further abstracted in the fly-base modules by
     # providing an option that only takes packages as input, and adds the
     # layer. This would provide more isolation from the implementation details.
