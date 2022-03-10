878845e90960b75d44dc68963c446d310dfebea4
Samuel Dionne-Riel <samuel.dionne-riel@determinate.systems>
2022-03-10 14:57:57 -0500

    default.nix: Show how to use the layers in the local config

--- default.nix
+++ default.nix
@@ -1,9 +1,20 @@
 { fly-base ? (import ./fly-base.nix {})
 }:
 (fly-base.fly.evalSpec) {
-  config = {
+  config = { pkgs, ... }: {
     templates.rails.enable = true;
     app.source = builtins.fetchGit ./.;
     runtimes.ruby.version = "3.1.1";
+
+    # This could also be further abstracted in the fly-base modules by
+    # providing an option that only takes packages as input, and adds the
+    # layer. This would provide more isolation from the implementation details.
+    container.additionalLayers = [
+      {
+        contents = with pkgs; [
+          ffmpeg
+        ];
+      }
+    ];
   };
 }
